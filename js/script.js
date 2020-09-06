3624f0fe1af1a556e1ce8b093679fd1a5bcc205b
KotaIshimatsu <meerat0k.g@gmail.com>
2020-09-06 15:08:20 +0900

    テキストをスプレットシートに反映

--- js/script.js
+++ js/script.js
@@ -1,35 +1,47 @@
 function renderTimeline(data) {
-    $.each(data, function(_index, element) {
-        $('main').append(
-            `
-                <div class="timeline">
-                    <a href="#">
-                        <img src="https://abs.twimg.com/sticky/default_profile_images/default_profile_reasonably_small.png" alt="">
-                    </a>
-                    <div class="timeline-contents">
-                        <div class="user-information">
-                            <a href="#" class="user-name">
-                                ${element.userName}
-                            </a>
-                            <span class="user-id">
-                                ${element.userId}
-                            </span>
-                        </div>
-                        <div class="tweet-text">
-                            ${element.tweet}
-                        </div>
+    $('main').append(
+        `
+            <div class="timeline">
+                <a href="#">
+                    <img src="https://abs.twimg.com/sticky/default_profile_images/default_profile_reasonably_small.png" alt="">
+                </a>
+                <div class="timeline-contents">
+                    <div class="user-information">
+                        <a href="#" class="user-name">
+                            ${data.userName}
+                        </a>
+                        <span class="user-id">
+                            ${data.userId}
+                        </span>
+                    </div>
+                    <div class="tweet-text">
+                        ${data.tweet}
                     </div>
                 </div>
-            `
-        );
-    })
+            </div>
+        `
+    );
 }
+
+$('.tweet-btn').on('click', function() {
+    var textValue = $('#text').val()
+    $.ajax({
+        url:`https://script.google.com/macros/s/AKfycbye6MAPdesaKexQchHG_G3C9yAkWkrEsW-NA69CD0VMIebgH9Y/exec?type=post&userid=hhh&username=kkk&tweet=${textValue}`,
+        
+    })
+    .done(  (result) => {
+        renderTimeline(result.data)
+        $('#text').val('')
+    })
+})
 
 $(function() {
     $.ajax({
-        url:'https://script.googleusercontent.com/macros/echo?user_content_key=xQ4E_BGsTjUHbkA2uNzsPVF_kFsEdKI3zKXRju9rRSzNVARhAcR4PfWfxZBtb9gyOExgy-tv0ZulRNUXlsWUHt9-sB-1ExcQm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnKhYSpijLKm9ry_16E7dOXB2Ll1leWt5x4qy7dquTyYZuvmKQVUbD-cImi6t7tipiOIqWsDnSrEd&lib=Mqjqe7vZuLu6mVtNW2QQbP-Yg69CV_lri'
+        url:'https://script.google.com/macros/s/AKfycbye6MAPdesaKexQchHG_G3C9yAkWkrEsW-NA69CD0VMIebgH9Y/exec'
     })
     .done( (result) => {
-        renderTimeline(result.data)
+        $.each(result.data, function(_index, element) {
+            renderTimeline(element)
+        })
     })
 })
